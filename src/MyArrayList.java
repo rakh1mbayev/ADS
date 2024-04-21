e15558dbfa8c3c0ade55883f3b7ef5473c997a7a
Bekzhan <rakhimbayevbekzhan@gmail.com>
2024-04-21 20:17:42 +0500

    create MyArrayList create MyList

---
+++ src/MyArrayList.java
@@ -0,0 +1,158 @@
+import java.util.Iterator;
+
+class MyArrayList<T> implements MyList<T> {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T item) {
+        CapacityUp(size + 1);
+        elements[size++] = item;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        if (index < 0 || size <= index) {
+            throw new IndexOutOfBoundsException();
+        }
+        elements[index] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || size < index) {
+            throw new IndexOutOfBoundsException();
+        }
+        CapacityUp(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(size, item);
+    }
+
+    @Override
+    public T get(int index) {
+        check(index);
+        return (T) elements[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        check(index);
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        return indexOf(object);
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        System.arraycopy(elements, 0, array, 0, size);
+        return array;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void CapacityUp(int minCapacity) {
+        if (minCapacity > elements.length) {
+            int newCapacity = Math.max(elements.length * 2, minCapacity);
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements, 0, size);
+            elements = newElements;
+        }
+    }
+
+    private void check(int index) {
+        if (index < 0 || size <= index) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException();
+                }
+                return (T) elements[currentIndex++];
+            }
+        };
+    }
+
+    // Implement other methods from MyList interface...
+}
